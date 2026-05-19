835a8ca0c8dcf85a915a8c6b478c351fbaca5050
Brad Brown Jr. <bradbrownjr@gmail.com>
2026-05-19 12:13:41 +0000

    v0.25.60: fix missing cd_add_list_type translations

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "io.github.bradbrownjr.tangible"
         minSdk = 26 // Android 8.0
         targetSdk = 35
-        versionCode = 130
-        versionName = "0.25.59"
+        versionCode = 131
+        versionName = "0.25.60"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
--- server/src/tangible/__init__.py
+++ server/src/tangible/__init__.py
@@ -1,3 +1,3 @@
 """Tangible server package."""
 
-__version__ = "0.25.59"
+__version__ = "0.25.60"
